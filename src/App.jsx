c4cff405b5883d145918ad638f26efc88c4c2260
Myllena Bitar <myllena.odo@gmail.com>
2024-11-19 22:13:20 -0300

    add font, media query

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
 return (
 <div>
-  <h1>Preferência:{produto}</h1>
+  <h1>Preferência: {produto}</h1>
   <button onClick={handleClick} style={{marginRight:'1rem'}}>Notebook</button>
   <button onClick={handleClick} style={{marginRight:'1rem'}}>Smartphone</button>
   <button onClick={handleClick} style={{marginRight:'1rem'}}>Smartwatch</button>
--- src/Produto.jsx
+++ src/Produto.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Produto = ({ produto }) => {
+const Produto = ({ produto }) => {          
     const [dados, setDados] = React.useState();
 
     React.useEffect(() => {
